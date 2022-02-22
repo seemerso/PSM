a26728f7ddd8a01210fb444477ad3431678c49b8
seemerso <97478850+seemerso@users.noreply.github.com>
2022-02-21 23:39:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var products = [
   {
     id: 0,
     name: "Apples",
-    price: "2.99",
+    price: "$2.99",
     image:
       "https://banner2.kisspng.com/20180310/jyw/kisspng-plum-tomato-apple-fuji-vector-two-apples-5aa49732abbf96.2950483615207360507035.jpg",
     color: "red"
@@ -10,7 +10,7 @@ var products = [
   {
     id: 1,
     name: "Bananas",
-    price: "2.50",
+    price: "$2.50",
     image:
       "https://icon2.kisspng.com/20180125/cyq/kisspng-banana-powder-fruit-cavendish-banana-banana-5a6a705e555d06.3110857015169250223497.jpg",
     color: "gold"
@@ -18,7 +18,7 @@ var products = [
   {
     id: 2,
     name: "Oranges",
-    price: "3.50",
+    price: "$3.50",
     image:
       "https://icon2.kisspng.com/20180308/wlw/kisspng-south-africa-blood-orange-mandarin-orange-tangelo-south-africa-imports-of-oranges-5aa10d936cc6c7.6226563815205042114456.jpg",
     color: "orange"
@@ -30,7 +30,7 @@ $.each(products, function(i, value) {
   product.addClass("product");
   product.append("<img src='" + value.image + "'/>");
   product.append("<div> Name: " + value.name + "</div>");
-  product.append("<div> Price: $" + value.price + "</div>");
+  product.append("<div> Price: " + value.price + "</div>");
   
   $(product).attr("color", value.color);
   $(product).attr("name", value.name);
